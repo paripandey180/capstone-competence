868e8879a370863d1fc6eac529120ccb3ef2d984
paripandey180 <paripandey180@gmail.com>
2025-10-17 09:51:02 -0700

    feature: view your expenses

--- expense_tracker.py
+++ expense_tracker.py
@@ -7,16 +7,29 @@ def add_expense():
     expenses.append({"name": name, "amount": amount, "category": category})
     print(f"✅ Added {name} (${amount}) to {category} category.\n")
 
+def view_expense():
+    if not expenses:
+        print("You have not recorded any expenses yet.\n")
+        return
+    else:
+        print("📋 Here are your expenses:")
+        for expense in expenses:
+            print(f"- {expense['name']}: ${expense['amount']} ({expense['category']})")
+        print()
+
 def main():
     while True:
         print("Expense Tracker Menu:")
         print("1. Add Expense")
-        print("2. Exit")
+        print("2. View Expenses")
+        print("3. Exit")
         choice = input("Choose an option: ")
 
         if choice == "1":
             add_expense()
         elif choice == "2":
+            view_expense()
+        elif choice == "3":
             print("Exiting... Goodbye!")
             break
         else:
